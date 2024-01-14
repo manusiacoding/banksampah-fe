28f55628fe8f5bd25816d1b4461f791fec61bf21
Fawwaz Hudzalfah Saputra <manusiacoding@linux.manusiacoding>
2024-01-14 12:15:32 +0700

    make login page

--- src/components/AppSidebar.js
+++ src/components/AppSidebar.js
@@ -16,34 +16,34 @@ import 'simplebar/dist/simplebar.min.css'
 import navigation from '../_nav'
 
 const AppSidebar = () => {
-  const dispatch = useDispatch()
-  const unfoldable = useSelector((state) => state.sidebarUnfoldable)
-  const sidebarShow = useSelector((state) => state.sidebarShow)
+    const dispatch = useDispatch()
+    const unfoldable = useSelector((state) => state.sidebarUnfoldable)
+    const sidebarShow = useSelector((state) => state.sidebarShow)
 
-  return (
-    <CSidebar
-      position="fixed"
-      unfoldable={unfoldable}
-      visible={sidebarShow}
-      onVisibleChange={(visible) => {
-        dispatch({ type: 'set', sidebarShow: visible })
-      }}
-    >
-      <CSidebarBrand className="d-none d-md-flex" to="/">
-        <CIcon className="sidebar-brand-full" icon={logoNegative} height={35} />
-        <CIcon className="sidebar-brand-narrow" icon={sygnet} height={35} />
-      </CSidebarBrand>
-      <CSidebarNav>
-        <SimpleBar>
-          <AppSidebarNav items={navigation} />
-        </SimpleBar>
-      </CSidebarNav>
-      <CSidebarToggler
-        className="d-none d-lg-flex"
-        onClick={() => dispatch({ type: 'set', sidebarUnfoldable: !unfoldable })}
-      />
-    </CSidebar>
-  )
+    return (
+        <CSidebar
+            position="fixed"
+            unfoldable={unfoldable}
+            visible={sidebarShow}
+            onVisibleChange={(visible) => {
+                dispatch({ type: 'set', sidebarShow: visible })
+            }}
+        >
+            <CSidebarBrand className="d-none d-md-flex" to="/">
+                <CIcon className="sidebar-brand-full" icon={logoNegative} height={35} />
+                <CIcon className="sidebar-brand-narrow" icon={sygnet} height={35} />
+            </CSidebarBrand>
+            <CSidebarNav>
+                <SimpleBar>
+                    <AppSidebarNav items={navigation} />
+                </SimpleBar>
+            </CSidebarNav>
+            <CSidebarToggler
+                className="d-none d-lg-flex"
+                onClick={() => dispatch({ type: 'set', sidebarUnfoldable: !unfoldable })}
+            />
+        </CSidebar>
+    )
 }
 
 export default React.memo(AppSidebar)
--- src/views/pages/login/Login.js
+++ src/views/pages/login/Login.js
@@ -1,86 +1,80 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import {
-  CButton,
-  CCard,
-  CCardBody,
-  CCardGroup,
-  CCol,
-  CContainer,
-  CForm,
-  CFormInput,
-  CInputGroup,
-  CInputGroupText,
-  CRow,
+    CButton,
+    CCard,
+    CCardBody,
+    CCardGroup,
+    CCol,
+    CContainer,
+    CForm,
+    CFormInput,
+    CInputGroup,
+    CInputGroupText,
+    CRow,
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
 import { cilLockLocked, cilUser } from '@coreui/icons'
 
 const Login = () => {
-  return (
-    <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
-      <CContainer>
-        <CRow className="justify-content-center">
-          <CCol md={8}>
-            <CCardGroup>
-              <CCard className="p-4">
-                <CCardBody>
-                  <CForm>
-                    <h1>Login</h1>
-                    <p className="text-medium-emphasis">Sign In to your account</p>
-                    <CInputGroup className="mb-3">
-                      <CInputGroupText>
-                        <CIcon icon={cilUser} />
-                      </CInputGroupText>
-                      <CFormInput placeholder="Username" autoComplete="username" />
-                    </CInputGroup>
-                    <CInputGroup className="mb-4">
-                      <CInputGroupText>
-                        <CIcon icon={cilLockLocked} />
-                      </CInputGroupText>
-                      <CFormInput
-                        type="password"
-                        placeholder="Password"
-                        autoComplete="current-password"
-                      />
-                    </CInputGroup>
-                    <CRow>
-                      <CCol xs={6}>
-                        <CButton color="primary" className="px-4">
-                          Login
-                        </CButton>
-                      </CCol>
-                      <CCol xs={6} className="text-right">
-                        <CButton color="link" className="px-0">
-                          Forgot password?
-                        </CButton>
-                      </CCol>
-                    </CRow>
-                  </CForm>
-                </CCardBody>
-              </CCard>
-              <CCard className="text-white bg-primary py-5" style={{ width: '44%' }}>
-                <CCardBody className="text-center">
-                  <div>
-                    <h2>Sign up</h2>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                      tempor incididunt ut labore et dolore magna aliqua.
-                    </p>
-                    <Link to="/register">
-                      <CButton color="primary" className="mt-3" active tabIndex={-1}>
-                        Register Now!
-                      </CButton>
-                    </Link>
-                  </div>
-                </CCardBody>
-              </CCard>
-            </CCardGroup>
-          </CCol>
-        </CRow>
-      </CContainer>
-    </div>
-  )
+    return (
+        <div className='bg-body-tertiary' style={{ overflowX: 'hidden' }}>
+            <CRow className='min-vh-100'>
+                <CCardGroup>
+                    <CCard className="text-white bg-primary py-5 mt-3 mt-md-0 mx-3 mx-md-0 mb-3 mb-md-0">
+                        <CCardBody className="text-center d-flex align-items-center justify-content-center">
+                            <div>
+                                <h2>Sign up</h2>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                    tempor incididunt ut labore et dolore magna aliqua.
+                                </p>
+                                <Link to="/register">
+                                    <CButton color="primary" className="mt-3" active tabIndex={-1}>
+                                        Register Now!
+                                    </CButton>
+                                </Link>
+                            </div>
+                        </CCardBody>
+                    </CCard>
+                    <CCard className='mx-3 mx-md-0'>
+                        <CCardBody className='d-flex align-items-center justify-content-center'>
+                            <CForm className='w-100'>
+                                <h1>Login</h1>
+                                <p className="text-body-secondary">Sign In to your account</p>
+                                <CInputGroup className="mb-3">
+                                    <CInputGroupText>
+                                        <CIcon icon={cilUser} />
+                                    </CInputGroupText>
+                                    <CFormInput placeholder="Username" autoComplete="username" />
+                                </CInputGroup>
+                                <CInputGroup className="mb-4">
+                                    <CInputGroupText>
+                                        <CIcon icon={cilLockLocked} />
+                                    </CInputGroupText>
+                                    <CFormInput
+                                        type="password"
+                                        placeholder="Password"
+                                        autoComplete="current-password"
+                                    />
+                                </CInputGroup>
+                                <CRow>
+                                    <CCol className='d-flex justify-content-end'>
+                                        <CButton color="link" className="mr-5">
+                                            Forgot password?
+                                        </CButton>
+                                        <CButton color="primary" className="px-4">
+                                            Login
+                                        </CButton>
+                                    </CCol>
+                                </CRow>
+                            </CForm>
+                        </CCardBody>
+                    </CCard>
+                </CCardGroup>
+            </CRow>
+        </div>
+    )
 }
 
 export default Login
